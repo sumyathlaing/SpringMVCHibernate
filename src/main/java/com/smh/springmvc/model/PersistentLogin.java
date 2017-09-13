dd0f3c7381cd20652af9a518c5cf2b21c540d7ad
su <su.myat.hlaing@frobom.com>
2017-09-13 15:03:42 +0630

    updated entity with auto-create

--- src/main/java/com/smh/springmvc/model/PersistentLogin.java
+++ src/main/java/com/smh/springmvc/model/PersistentLogin.java
@@ -1,11 +1,14 @@
 package com.smh.springmvc.model;
 
 import java.io.Serializable;
+import java.util.Calendar;
 import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -16,11 +19,15 @@ public class PersistentLogin implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @Id
-    private String series;
+    private static final int EXPIRATION = 60 * 24;
 
-    @Column(name = "USERNAME", unique = true, nullable = false)
-    private String username;
+    @Id
+    @OneToOne
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    @Column(name = "SERIES", unique = true, nullable = false)
+    private String series;
 
     @Column(name = "TOKEN", unique = true, nullable = false)
     private String token;
@@ -28,20 +35,39 @@ public class PersistentLogin implements Serializable {
     @Temporal(TemporalType.TIMESTAMP)
     private Date last_used;
 
+    public PersistentLogin() {
+        super();
+    }
+
+    public PersistentLogin(String token, User user) {
+        super();
+
+        this.token = token;
+        this.user.setSsoId(user.getSsoId());
+        this.last_used = calculateExpiryDate(EXPIRATION);
+    }
+
+    private Date calculateExpiryDate(int expiryTimeInMinutes) {
+        final Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(new Date().getTime());
+        cal.add(Calendar.MINUTE, expiryTimeInMinutes);
+        return new Date(cal.getTime().getTime());
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
     public String getSeries() {
         return series;
     }
 
     public void setSeries(String series) {
         this.series = series;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
     }
 
     public String getToken() {
--- src/main/java/com/smh/springmvc/model/UserProfile.java
+++ src/main/java/com/smh/springmvc/model/UserProfile.java
@@ -10,15 +10,16 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="USER_PROFILE")
+@Table(name = "USER_PROFILE")
 public class UserProfile implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    
-    @Column(name="TYPE", length=15, unique=true, nullable=false)
+
+    @Column(name = "TYPE", length = 15, unique = true, nullable = false)
     private String type = UserProfileType.USER.getUserProfileType();
 
     public Integer getId() {
@@ -36,7 +37,7 @@ public class UserProfile implements Serializable {
     public void setType(String type) {
         this.type = type;
     }
-    
+
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -45,7 +46,7 @@ public class UserProfile implements Serializable {
         result = prime * result + ((type == null) ? 0 : type.hashCode());
         return result;
     }
-    
+
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
@@ -67,7 +68,7 @@ public class UserProfile implements Serializable {
             return false;
         return true;
     }
- 
+
     @Override
     public String toString() {
         return "UserProfile [id=" + id + ", type=" + type + "]";
